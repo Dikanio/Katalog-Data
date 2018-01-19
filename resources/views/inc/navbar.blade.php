68b595b146479e9128336c074bea2489fac65a9b
fmz620 <mzfahri620@gmail.com>
2018-01-19 15:02:50 +0700

    perbaikan tampilan notifikasi, login dan session

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -17,7 +17,6 @@
     </ul>
     <ul class="nav navbar-nav navbar-right">
         @if (Route::has('login'))
-            <div class="top-right links">
                 @auth
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
@@ -42,7 +41,6 @@
                     <a href="{{ route('login') }}">Login</a>
                     <a href="{{ route('register') }}">Register</a>
                 @endauth
-            </div>
         @endif
     </ul>
     </div><!--/.nav-collapse -->
